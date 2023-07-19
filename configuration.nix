96f9a8d6d79499d68f95128fbbf8af5f26cbafce
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2023-07-19 21:08:20 +0200

    Fix SDDM

--- configuration.nix
+++ configuration.nix
@@ -42,10 +42,8 @@ in
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.autoNumlock = true;
-  environment.plasma5.excludePackages = {
-    enable = true;
-    excludePackages = with pkgs.libsForQt5; [ elisa ];
-  };
+  services.xserver.desktopManager.plasma5.enable = true;
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [ elisa ];
 
   # Configure keymap in X11
   services.xserver = {
