30699cefe37f6c5a2ca6463cd877fde333ef56f4
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-01-20 00:00:16 +0100

    Sublime Text: move `font` type to `utils.nix`

--- new/modules/sublime-text.nix
+++ new/modules/sublime-text.nix
@@ -3,6 +3,7 @@ with lib;
 let
   cfg = config.programs.sublime-text;
   jsonFormat = pkgs.formats.json { };
+  fontType = (import ./utils.nix { inherit lib; }).types.font;
   pluginOptions = types.submodule ({ config, ... }: {
     options = {
       managed = mkOption {
@@ -43,45 +44,7 @@ in
         Sublime Text's user settings.
       '';
     };
-    font = {
-      package = mkOption {
-        type = types.nullOr types.package;
-        default = null;
-        example = literalExpression "pkgs.fira-code";
-        description = ''
-          Package providing the font. This package will be installed
-          to your profile. If `null` then the font
-          is assumed to already be available in your profile.
-        '';
-      };
-
-      name = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        example = "Fira Code";
-        description = ''
-          The family name of the font within the package.
-        '';
-      };
-
-      size = mkOption {
-        type = types.nullOr types.number;
-        default = null;
-        example = "8";
-        description = ''
-          The size of the font.
-        '';
-      };
-
-      features = mkOption {
-        type = types.listOf types.str;
-        default = [ ];
-        example = ''[ "subpixel_antialias" "ss03" "ss05" ]'';
-        description = ''
-          The font features to enable.
-        '';
-      };
-    };
+    font = fontType;
     keymap = mkOption {
       type = jsonFormat.type;
       default = [ ];
---
+++ new/modules/utils.nix
@@ -0,0 +1,42 @@
+{ lib, ... }:
+with lib; {
+  types.font = {
+    package = mkOption {
+      type = types.nullOr types.package;
+      default = null;
+      example = literalExpression "pkgs.fira-code";
+      description = ''
+        Package providing the font. This package will be installed
+        to your profile. If `null` then the font
+        is assumed to already be available in your profile.
+      '';
+    };
+
+    name = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      example = "Fira Code";
+      description = ''
+        The family name of the font within the package.
+      '';
+    };
+
+    size = mkOption {
+      type = types.nullOr types.number;
+      default = null;
+      example = "8";
+      description = ''
+        The size of the font.
+      '';
+    };
+
+    features = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      example = ''[ "subpixel_antialias" "ss03" "ss05" ]'';
+      description = ''
+        The font features to enable.
+      '';
+    };
+  };
+}
