2f44fb74651967f3210640f49e1ac904837df43c
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-01-20 15:17:15 +0100

    NewConfig: use nerdfont version of fira code

--- new/config/globals.nix
+++ new/config/globals.nix
@@ -1,8 +1,8 @@
 { pkgs, self, ... }: rec {
   font = {
     monospace = {
-      package = pkgs.fira-code;
-      name = "Fira Code";
+      package = pkgs.fira-code-nerdfont;
+      name = "FiraCode Nerd Font";
       features = [
         "subpixel_antialias"
         "ss03"
