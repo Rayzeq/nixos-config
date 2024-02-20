a5bd0cf1eccd18794f74465320bcf120d81e0121
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-02-20 14:18:13 +0100

    kde: remove konsole

--- configuration.nix
+++ configuration.nix
@@ -44,7 +44,7 @@ in
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.desktopManager.plasma5.enable = true;
-  environment.plasma5.excludePackages = with pkgs.libsForQt5; [ elisa ];
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [ elisa konsole ];
 
   services.logind.extraConfig = ''
     HandleLidSwitch=hibernate
