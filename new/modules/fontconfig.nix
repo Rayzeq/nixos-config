0097a8b5897a50f77e6dd749f042f5fdfc21f505
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-08-24 11:25:56 +0200

    bettermanager: add fontconfig module

---
+++ new/modules/fontconfig.nix
@@ -0,0 +1,18 @@
+{ lib, globals, ... }:
+{
+  config = {
+    home.packages = (
+      map
+        (f: f.package)
+        (globals.font.sans.fallbacks ++ globals.font.monospace.fallbacks)
+    ) ++ lib.optional (globals.font.sans.package != null) globals.font.sans.package
+    ++ lib.optional (globals.font.monospace.package != null) globals.font.monospace.package;
+    fonts.fontconfig = {
+      enable = true;
+      defaultFonts = {
+        sansSerif = [ globals.font.sans.name ] ++ map (f: f.name) globals.font.sans.fallbacks;
+        monospace = [ globals.font.monospace.name ] ++ map (f: f.name) globals.font.monospace.fallbacks;
+      };
+    };
+  };
+}
