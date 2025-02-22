bfbfe3caf55593496baf9e8551c813ff57792aac
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2025-02-22 19:26:40 +0100

    kde: remove more unused packages

--- configuration.nix
+++ configuration.nix
@@ -62,7 +62,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [ elisa konsole ];
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [ elisa konsole kate akregator spectacle ];
 
   xdg.portal = {
     enable = true;
