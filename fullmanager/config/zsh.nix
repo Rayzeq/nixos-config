355f2ca9458deb776ade6a3e4d3ab0d651c8cf33
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2025-10-16 20:21:32 +0200

    zsh: simplify `system-update` alias by using `--upgrade-all`

--- fullmanager/config/zsh.nix
+++ fullmanager/config/zsh.nix
@@ -68,7 +68,7 @@
       clear = "clear -T xterm-256color";
       dust = "sudo dust -rx";
       discord = "discord --enable-features=UseOzonePlatform --ozone-platform=wayland";
-      system-update = "sudo zsh -c \"nix-channel --update home-manager && nixos-rebuild switch --upgrade\"";
+      system-update = "sudo nixos-rebuild switch --upgrade-all";
       nixos-cleanup = "nix-collect-garbage -d; sudo zsh -c \"nix-collect-garbage -d && nixos-rebuild boot\"; echo \"\\nGC Roots:\\n\"; ls -l /nix/var/nix/gcroots/auto/";
     };
   };
