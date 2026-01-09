296b221d478dfc2642357a029d500642e834f444
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2026-01-10 00:15:12 +0100

    fix: enable numlock on greeter

--- fullmanager/config/greetd.nix
+++ fullmanager/config/greetd.nix
@@ -1,12 +1,10 @@
 { pkgs, ... }: {
   greetd = {
     enable = true;
+    enableNumlock = true;
     useTextGreeter = true;
     settings = {
-      default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --user-menu --user-menu-min-uid 1000 --user-menu-max-uid 1001 --cmd start-hyprland";
-        user = "greeter";
-      };
+      default_session.command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --user-menu --user-menu-min-uid 1000 --user-menu-max-uid 1001 --cmd start-hyprland";
     };
   };
 }
--- fullmanager/modules/greetd.nix
+++ fullmanager/modules/greetd.nix
@@ -1,5 +1,6 @@
 { nixpkgs, lib, pkgs, config, ... }:
 let
+  inherit (lib) mkOption mkIf types;
   cfg = config.greetd;
 
   greetdOptions = (import "${nixpkgs}/nixos/modules/services/display-managers/greetd.nix" {
@@ -10,7 +11,18 @@ in
 {
   options.greetd = {
     inherit (greetdOptions) enable package settings useTextGreeter;
+
+    enableNumlock = mkOption {
+      type = types.bool;
+    };
   };
 
-  config.system.services.greetd = lib.mkIf cfg.enable cfg;
+  config.system = mkIf cfg.enable {
+    services.greetd = {
+      inherit (cfg) enable package useTextGreeter settings;
+    };
+    systemd.services.greetd.preStart = mkIf
+      cfg.enableNumlock
+      "${pkgs.bash}/bin/sh -c '${pkgs.kbd}/bin/setleds -D +num < /dev/tty1'";
+  };
 }
