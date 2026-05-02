52067863796133f3447a82107109dc7af8941a6e
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2026-05-02 22:57:57 +0200

    prevent home-manager from including pkgs.kdePackages.breeze.qt5

--- config/desktop/qt.nix
+++ config/desktop/qt.nix
@@ -3,5 +3,7 @@
     enable = true;
     platformTheme.name = "kde";
     style.name = "Breeze";
+    # prevent home-manager from including pkgs.kdePackages.breeze.qt5
+    style.package = pkgs.kdePackages.breeze;
   };
 }
