2c359e27406f76a46f4a2e597ebd2f3e7ba79bf2
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2026-05-04 18:46:39 +0200

    refactor: move remaining legacy hyprland config to workarounds branch

--- _legacy/hyprland/user.nix
+++ _legacy/hyprland/user.nix
@@ -2,14 +2,5 @@
 {
   home-manager.users.zacharie = { config, ... }: {
     services.playerctld.enable = true;
-    wayland.windowManager.hyprland = {
-      settings = {
-        exec-once = [
-          "eww open statusbar && ( nm-applet & blueman-applet & discord --enable-features=UseOzonePlatform --ozone-platform=wayland --start-minimized & )"
-        ];
-
-        env = lib.mapAttrsToList (name: value: name + "," + (toString value)) (config.systemd.user.sessionVariables // config.home.sessionVariables);
-      };
-    };
   };
 }
