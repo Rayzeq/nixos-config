2fa20d5453d71455d8e2bc338ee97d6ab81f4ddd
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2026-05-09 12:47:59 +0200

    fix: make root shells work with kitty

--- users/root/default.nix
+++ users/root/default.nix
@@ -4,5 +4,7 @@
     ../../config/desktop/fonts.nix
     ../../config/programs/development/sublime-text
     ../../config/programs/development/sublime-merge.nix
+    # needed to provide shell definitions for xterm-kitty
+    ../../config/desktop/kitty.nix
   ];
 }
