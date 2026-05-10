0380e46c90b7a96fbb5d5f2237ddb072e58c63b5
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2026-05-10 17:03:18 +0200

    feat: add ssh key to zacharie (openssh still disabled by default)

--- users/zacharie/default.nix
+++ users/zacharie/default.nix
@@ -8,5 +8,7 @@
   system.age.identityPaths = [ "${hmConfig.home.homeDirectory}/.ssh/id_ed25519" ];
 
   nh.osFlake = "${hmConfig.xdg.configHome}/nixos";
+
+  user.openssh.authorizedKeys.keys = [ (import ../../secrets/keys.nix).zacharie ];
   pam.u2f.keys.zacharie = [ "T0NEtAQt/GfQDBcwVP38eaCqJB/F0sz5G38Hl2UrfDgntonIp0wSiRHl4ZIgjR03qAnBxX8aO/8DPPzgIKiQjg==,+1Zu7GGqcP7gvxUgGhEtVOByh3wg6Lbpk6/RCRDtF+5UgoR3E7FF2xtwEGvqviURrbayFj26hIeaV71SUpRQqg==,es256,+presence" ];
 }
