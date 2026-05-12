e22e2b81015977d6e3f00e498d09b24aad1d9159
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2026-05-12 22:01:18 +0200

    feat(sublime-text): replace pylsp with ruff and ty and move in a separate file

---
+++ config/programs/development/sublime-text/python.nix
@@ -0,0 +1,32 @@
+{
+  sublime-text = {
+    build-systems."Python - Terminus" = {
+      target = "terminus_open";
+      cancel = "terminus_cancel_build";
+      shell_cmd = ''python3 "''${file_path}/''${file_name}"; read -p "Press any key to continue..."'';
+      working_dir = "$folder";
+      selector = "source.python";
+    };
+    plugins = {
+      LSP-ruff.settings.initialization_options.settings = {
+        configurationPreference = "filesystemFirst";
+        configuration = {
+          target-version = "py314";
+          lint = {
+            future-annotations = true;
+            select = [ "ALL" ];
+            ignore = [
+              # Ignore missing copyright notice
+              "CPY"
+            ];
+          };
+          format = {
+            nested-string-quote-style = "preferred";
+            skip-magic-trailing-comma = true;
+          };
+        };
+      };
+      LSP-ty = { };
+    };
+  };
+}
