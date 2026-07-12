5fa8456273d3ad76de437315f3c1caefdee3b333
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2026-07-13 00:44:19 +0200

    feat(virtualization): add necessary packages for shared folders

--- config/programs/virtualization.nix
+++ config/programs/virtualization.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }: {
   system = {
-    virtualisation.libvirtd.enable = true;
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu.vhostUserPackages = [ pkgs.virtiofsd ];
+    };
     programs.virt-manager.enable = true;
     systemd.tmpfiles.rules = [ "L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware" ];
   };
