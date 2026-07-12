c1897fce3ea87a76c42caeb9995ed2c7c8fb9437
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2026-07-12 12:50:36 +0200

    add keybind to reset keyboard
    a

--- config/workarounds.nix
+++ config/workarounds.nix
@@ -1,4 +1,19 @@
 { pkgs, ... }: {
+  # Mouse keybind to reset keyboard
+  system.security.sudo.extraRules = [
+    {
+      groups = [ "wheel" ];
+      commands = [
+        {
+          command = "${pkgs.coreutils-full}/bin/tee /sys/bus/serio/drivers/atkbd/unbind";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
+  hypr.land.settings.bind = [
+    [ "F5" ''hl.dsp.exec_cmd("echo -n \"serio0\" | sudo ${pkgs.coreutils-full}/bin/tee /sys/bus/serio/drivers/atkbd/unbind")'' { long_press = true; locked = true; ignore_mods = true; dont_inhibit = true; submap_universal = true; } ]
+  ];
   hm = {
     # This is needed for dolphin to detect applications outside of plasma6.
     # This is technically expected behavior and (likely) won't be fixed from KDE's side,
