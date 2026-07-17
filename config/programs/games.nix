24f1a618862cf4c38c2d2feee60b4e812920081d
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2026-07-17 23:22:44 +0200

    feat: remove java8

--- config/programs/games.nix
+++ config/programs/games.nix
@@ -6,7 +6,10 @@
     localNetworkGameTransfers.openFirewall = true;
   };
 
-  hm.programs.prismlauncher.enable = true;
+  hm.programs.prismlauncher = {
+    enable = true;
+    package = pkgs.prismlauncher.override { jdks = with pkgs; [ jdk25 jdk21 jdk17 ]; };
+  };
   hm.home.packages = with pkgs; [
     heroic
   ];
